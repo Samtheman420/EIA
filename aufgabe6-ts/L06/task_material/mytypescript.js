0fea056ffce98ac656f9b5bc68b7ad541f4da05d
Samtheman420 <bouguilasami2002@gmail.com>
2022-12-01 14:24:27 +0100

    change

--- aufgabe6-ts/L06/task_material/mytypescript.js
+++ aufgabe6-ts/L06/task_material/mytypescript.js
@@ -26,4 +26,42 @@ console.log("Die EInwohnerzahl in" + spain + " ist in den letzen Jahren um " + P
 console.log("In" + italy + "beträgt die aktuelle Einwohnerzahl" + population2022IT);
 console.log("In" + italy + "betrug die Einwohnerzahl 2020" + population2020IT);
 console.log("Die EInwohnerzahl in" + italy + " ist in den letzen Jahren um " + ProzentIT + "% gestiegen.");
+//Deutschland
+const bevölkerungDE2022 = 83240000;
+const bevölkerungDE2006 = 82310000;
+//Frankreich
+const bevölkerungFR2022 = 67840000;
+const bevölkerungFR2006 = 63620000;
+//Italien
+const bevölkerungIT2022 = 58980000;
+const bevölkerungIT2006 = 58140000;
+//Spanien
+const bevölkerungP2022 = 10350000;
+const bevölkerungP2006 = 10520000;
+//EU
+const bevölkerungEU = 447700000;
+//allgemein
+var nation;
+var population22;
+var population06;
+function population(nation, population22, population06) {
+    document.querySelector("#people22").innerHTML = (population22 / 1000000).toFixed(2) + "Mio.";
+    document.querySelector("#text").innerHTML = "Bevölkerung in " + nation + " 2022";
+    document.querySelector("#relative").innerHTML = (population22 / bevölkerungEU * 100).toFixed(2) + "%";
+    document.querySelector("#rate").innerHTML = ((1 - population06 / population22) * 100).toFixed(2) + "%";
+    document.querySelector("#people06").innerHTML = ((population22 - population06) / 1000000) + "Mio.";
+    document.querySelector(".chart").style.height = (population22 / bevölkerungEU * 100) + "%";
+}
+document.querySelector(".germany").addEventListener('click', function () {
+    population("Deutschland", bevölkerungDE2022, bevölkerungDE2006);
+});
+document.querySelector(".france").addEventListener('click', function () {
+    population("Frankreich", bevölkerungFR2022, bevölkerungFR2006);
+});
+document.querySelector(".italy").addEventListener('click', function () {
+    population("Italien", bevölkerungIT2022, bevölkerungIT2006);
+});
+document.querySelector(".Spanien").addEventListener('click', function () {
+    population("Spanien", bevölkerungP2022, bevölkerungP2006);
+});
 //# sourceMappingURL=mytypescript.js.map
