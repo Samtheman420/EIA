cf0b7a11f00e72bf51399efdd2b6d4408240ec55
Samtheman420 <bouguilasami2002@gmail.com>
2022-12-02 14:27:13 +0100

    uff

--- aufgabe8/drumscript.js
+++ aufgabe8/drumscript.js
@@ -1,1 +1,14 @@
+function playSample(sound) {
+}
+const sound1 = new Audio('L08_task_material_assets_Keyboard_a.mp3.mp3');
+const sound2 = new Audio('L08_task_material_assets_Keyboard_c.mp3.mp3');
+const sound3 = new Audio('L08_task_material_assets_Keyboard_f.mp3.mp3');
+const sound4 = new Audio('L08_task_material_assets_Keyboard_d.mp3.mp3');
+const sound5 = new Audio('L08_task_material_assets_Keyboard_e.mp3.mp3');
+const sound6 = new Audio('L08_task_material_assets_DrumPad_snare.mp3.mp3');
+const sound7 = new Audio('L08_task_material_assets_DrumPad_laugh-1.mp3.mp3');
+const sound8 = new Audio('L08_task_material_assets_DrumPad_kick.mp3.mp3');
+const sound9 = new Audio('L08_task_material_assets_DrumPad_hihat.mp3.mp3');
+sound.play();
+document.querySelector();
 //# sourceMappingURL=drumscript.js.map
--- aufgabe8/drumscript.ts
+++ aufgabe8/drumscript.ts
@@ -1,1 +1,16 @@
- 
+ function playSample (sound) {
+
+ }
+
+    const sound1:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_a.mp3.mp3');
+    const sound2:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_c.mp3.mp3');
+    const sound3:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_f.mp3.mp3');
+    const sound4:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_d.mp3.mp3');
+    const sound5:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_e.mp3.mp3');
+    const sound6:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_snare.mp3.mp3');
+    const sound7:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_laugh-1.mp3.mp3');
+    const sound8:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_kick.mp3.mp3');
+    const sound9:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_hihat.mp3.mp3');
+sound.play();
+
+document.querySelector()
