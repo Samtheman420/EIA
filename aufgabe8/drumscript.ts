0b338f327aaa377b95535bc762c869b924db8eb0
Samtheman420 <bouguilasami2002@gmail.com>
2022-12-04 20:22:27 +0100

    type

--- aufgabe8/drumscript.ts
+++ aufgabe8/drumscript.ts
@@ -1,34 +1,54 @@
  
  
  
-function playSample (button) {
 
- }
  
 
  
-    const sound1:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_a.mp3.mp3');
-    const sound2:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_c.mp3.mp3');
-    const sound3:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_f.mp3.mp3');
-    const sound4:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_d.mp3.mp3');
-    const sound5:HTMLAudioElement = new Audio('L08_task_material_assets_Keyboard_e.mp3.mp3');
-    const sound6:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_snare.mp3.mp3');
-    const sound7:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_laugh-1.mp3.mp3');
-    const sound8:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_kick.mp3.mp3');
-    const sound9:HTMLAudioElement = new Audio('L08_task_material_assets_DrumPad_hihat.mp3.mp3');
-button.play( );
+    const sounda:HTMLAudioElement = new Audio('a.mp3.mp3')
+    const soundc:HTMLAudioElement = new Audio('c.mp3.mp3');
+    const soundf:HTMLAudioElement = new Audio('f.mp3.mp3');
+    const soundd:HTMLAudioElement = new Audio('d.mp3.mp3');
+    const sounde:HTMLAudioElement = new Audio('e.mp3.mp3');
+    const soundsnare:HTMLAudioElement = new Audio('snare.mp3.mp3');
+    const soundlaugh:HTMLAudioElement = new Audio('laugh.mp3.mp3');
+    const soundkick:HTMLAudioElement = new Audio('kick.mp3.mp3');
+    const soundhihat:HTMLAudioElement = new Audio('hihat.mp3.mp3');
 
 
+function playSample(sound){
+sound.play()
  
- 
+}
 
 
-document.querySelector("#button")
+document.querySelector(".a") .addEventListener("click",function(){
+    playSample(sounda)
+})
+document.querySelector(".c") .addEventListener("click",function(){
+    playSample(soundc)
+})
+document.querySelector(".f") .addEventListener("click",function(){
+    playSample(soundf)
+})
+document.querySelector(".d") .addEventListener("click",function(){
+    playSample(soundd)
+})
+document.querySelector(".e") .addEventListener("click",function(){
+    playSample(sounde)
+})
+document.querySelector(".snare") .addEventListener("click",function(){
+    playSample(soundsnare)
+})
+document.querySelector(".laugh") .addEventListener("click",function(){
+    playSample(soundlaugh)
+})
+document.querySelector(".kick") .addEventListener("click",function(){
+    playSample(soundkick)
+})
+document.querySelector(".hihat") .addEventListener("click",function(){
+    playSample(soundhihat)
+        
+})
 
 
- window.addEventListener("play", playSample);
-
-
-setInterval(function() {
-    // Anweisungen
- }, 500);
