d96d2409c70ab99da105d0a3fefa49a04d1d3644
Samtheman420 <bouguilasami2002@gmail.com>
2023-01-27 20:28:22 +0100

    bruh

--- Aufgabe10/script10.js
+++ Aufgabe10/script10.js
@@ -1,17 +1,68 @@
-let addToDoButton = document.getElementById('addtodo');
-let todocontainer = document.getElementById('todocontainer');
-let inputfield = document.getElementById('inputField');
-addToDoButton.addEventListener('click', function () {
-    let paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    todocontainer.appendChild(paragraph);
-    inputfield.value = "";
-    paragraph.addEventListener('click', function () {
-        paragraph.style.textDecoration = "line-through";
-    });
-    paragraph.addEventListener('dblclick', function () {
-        todocontainer.removeChild(paragraph);
-    });
+;
+let todos = [
+    {
+        text: "Lorem",
+        checked: true,
+    },
+    {
+        text: "Ipsum",
+        checked: false,
+    },
+    {
+        text: "Dolor",
+        checked: false,
+    },
+];
+let inputDOMElement;
+let addButtonDOMElement;
+let todosDOMElement;
+let counterDOMElement;
+window.addEventListener("load", function () {
+    inputDOMElement = document.querySelector("#inputTodo");
+    addButtonDOMElement = document.querySelector("#addButton");
+    todosDOMElement = document.querySelector("#todos");
+    counterDOMElement = document.querySelector("#counter");
+    addButtonDOMElement.addEventListener("click", addTodo);
+    drawListToDOM();
 });
+function drawListToDOM() {
+    todosDOMElement.innerHTML = "";
+    for (let index = 0; index < todos.length; index++) {
+        let todo = document.createElement("div");
+        todo.classList.add("todo");
+        todo.innerHTML = "<span class='check " + todos[index].checked + "'><i class='fas fa-check'></i></span>"
+            + todos[index].text +
+            "<span class='trash fas fa-trash-alt'></span>";
+        todo.querySelector(".check").addEventListener("click", function () {
+            toggleCheckState(index);
+        });
+        todo.querySelector(".trash").addEventListener("click", function () {
+            deleteTodo(index);
+        });
+        todosDOMElement.appendChild(todo);
+    }
+    updateCounter();
+}
+function updateCounter() {
+    counterDOMElement.innerHTML = todos.length + " in total";
+}
+function addTodo() {
+    if (inputDOMElement.value != "") {
+        const newtodo = {
+            text: inputDOMElement.value,
+            checked: false,
+        };
+        todos.unshift(newtodo);
+        inputDOMElement.value = "";
+        drawListToDOM();
+    }
+}
+function toggleCheckState(index) {
+    todos[index].checked = !todos[index].checked;
+    drawListToDOM();
+}
+function deleteTodo(index) {
+    todos.splice(index, 1);
+    drawListToDOM();
+}
 //# sourceMappingURL=script10.js.map
---
+++ Endabgabe/quiz.js
@@ -0,0 +1,108 @@
+class Quiz {
+    questions = [
+        {
+            question: "Wie heißt das HTML-Element, mit dem man einen Absatz erstellt?",
+            answers: ["p", "div", "a", "span"],
+            correctAnswer: "p"
+        },
+        {
+            question: "Wie heißt die Eigenschaft, mit der man die Hintergrundfarbe eines HTML-Elements festlegt?",
+            answers: ["color", "background", "background-color", "bgcolor"],
+            correctAnswer: "background-color"
+        },
+        {
+            question: "Wie heißt das HTML-Element, mit dem man einen Link erstellt?",
+            answers: ["a", "link", "url", "hyperlink"],
+            correctAnswer: "a"
+        },
+        {
+            question: "Wie heißt die Eigenschaft, mit der man die Schriftart eines HTML-Elements festlegt?",
+            answers: ["font-family", "font", "text-font", "typeface"],
+            correctAnswer: "font-family"
+        },
+        {
+            question: "Wie heißt das HTML-Element, mit dem man ein Bild einfügt?",
+            answers: ["img", "picture", "photo", "image"],
+            correctAnswer: "img"
+        },
+        {
+            question: "Wie heißt das Schlüsselwort, mit dem man eine Variable in TypeScript deklariert?",
+            answers: ["var", "let", "const", "declare"],
+            correctAnswer: "let"
+        },
+        {
+            question: "Wie heißt die Methode, mit der man die Größe eines Arrays in TypeScript herausfindet?",
+            answers: ["length()", "size()", "count()", "len()"],
+            correctAnswer: "length"
+        },
+        {
+            question: "Wie heißt das Schlüsselwort, mit dem man eine Funktion in TypeScript deklariert?",
+            answers: ["function", "func", "method", "procedure"],
+            correctAnswer: "function"
+        },
+        {
+            question: "Wie heißt das Schlüsselwort, mit dem man eine Klasse in TypeScript deklariert?",
+            answers: ["class", "type", "constructor", "object"],
+            correctAnswer: "class"
+        },
+        {
+            question: "Wie heißt der Typ, der verwendet wird, um einen Wert darzustellen, der entweder true oder false sein kann?",
+            answers: ["boolean", "bool", "logical", "true/false"],
+            correctAnswer: "boolean"
+        },
+    ];
+    currentQuestionIndex = 0;
+    score = 0;
+    targetScore = 3;
+    startQuiz() {
+        this.renderQuestion();
+    }
+    renderQuestion() {
+        const question = this.questions[this.currentQuestionIndex];
+        const questionEl = document.querySelector("#question");
+        questionEl.innerText = question.question;
+        const answersEl = document.querySelector("#answers");
+        answersEl.innerHTML = "";
+        for (const answer of question.answers) {
+            const answerEl = document.createElement("div");
+            answerEl.innerText = answer;
+            answerEl.addEventListener("click", () => this.checkAnswer(answer));
+            answersEl.appendChild(answerEl);
+        }
+    }
+    checkAnswer(answer) {
+        const question = this.questions[this.currentQuestionIndex];
+        if (answer === question.correctAnswer) {
+            alert("BRAVOOO!");
+        }
+        else {
+            alert("BRUUUH.");
+        }
+        this.currentQuestionIndex++;
+        if (this.currentQuestionIndex === this.questions.length) {
+            alert("Quiz abgeschlossen!");
+        }
+        else {
+            this.renderQuestion();
+        }
+    }
+}
+const topicSelector = document.querySelector("#topic-selector");
+topicSelector.addEventListener("change", (event) => {
+    const selectedValue = event.target.value;
+    getQuestion(selectedValue);
+    const topicSelector = document.getElementById("topic-selector");
+    topicSelector.addEventListener("change", (event) => {
+        const selectedValue = event.target.value;
+        getQuestion(selectedValue);
+    });
+});
+var currentScore = 0;
+function updateScore() {
+    currentScore++;
+    document.getElementById("score").innerHTML = "Score: " + currentScore;
+}
+const quiz = new Quiz();
+quiz.startQuiz();
+document.getElementById("next-button").addEventListener("click", () => quiz.displayNextQuestion());
+//# sourceMappingURL=quiz.js.map
